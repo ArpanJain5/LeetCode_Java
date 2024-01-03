7af802fa11a04d9b6c679c1e54538bb6a211f02f
Arpan-Jain <91553265+ArpanJain5@users.noreply.github.com>
2024-01-03 12:04:21 +0530

    Time: 20 ms (17.86%), Space: 44.5 MB (40.67%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int numberOfBeams(String[] bank) {
+    var onesA = 0;
+    var cnt = 0;
+    for (var row : bank) {
+      var onesB = 0;
+      for (var c : row.toCharArray())
+        if (c == '1') onesB++;
+      if (onesB == 0) continue;
+      cnt += onesA * onesB;
+      onesA = onesB;
+    }
+    return cnt;
+  }
+}
