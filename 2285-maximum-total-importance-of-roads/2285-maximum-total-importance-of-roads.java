c54a81f904e5b3bb51b704f7cb800a63d05bef6b
Arpan-Jain <91553265+ArpanJain5@users.noreply.github.com>
2024-06-28 04:45:57 -0300

    Time: 29 ms (35.14%), Space: 66.9 MB (29.19%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        int[] counts = new int[n];
+        for(int[] road : roads){
+            counts[road[0]]++;
+            counts[road[1]]++;
+        }
+        Queue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < n ; i++){
+            pq.add(counts[i]);
+        }
+        long score = 0; int assignedNumber = 1;
+        while(!pq.isEmpty()){
+            int times = pq.poll();
+            score += ((long)assignedNumber * times);
+            assignedNumber++;
+        }
+        return score;
+    }
+}
